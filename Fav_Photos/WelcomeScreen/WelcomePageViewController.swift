1e43d4ffcf68a857edd36327266ce3dfad1c71ae
Emmanuel Obuh <81360481+emmanuelroyal@users.noreply.github.com>
2021-07-28 17:24:14 +0100

    feat: added WelcomePage animations

--- Fav_Photos/WelcomeScreen/WelcomePageViewController.swift
+++ Fav_Photos/WelcomeScreen/WelcomePageViewController.swift
@@ -11,8 +11,13 @@ import GoogleSignIn
 
 class WelcomePageViewController: UIViewController, LoginButtonDelegate {
     
+    @IBOutlet weak var signIN: UIButton!
+    @IBOutlet weak var socialLogins: UIStackView!
     @IBOutlet weak var fbSignIn: FBLoginButton!
+    @IBOutlet weak var welcomeLabel: UILabel!
     
+    @IBOutlet weak var signUp: UIButton!
+    @IBOutlet weak var smallLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         setNavBar()
@@ -22,8 +27,37 @@ class WelcomePageViewController: UIViewController, LoginButtonDelegate {
         }
         else { fbSignIn.permissions = ["public_profile", "email"]
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.signIN.center.y += 5
+        self.signUp.center.y += 5
+        self.socialLogins.center.y += 5
+        self.welcomeLabel.center.x -= 10
+        self.smallLabel.center.x -= 10
+        signIN.alpha = 0.2
+        signUp.alpha = 0.2
+        socialLogins.alpha = 0.0
+        self.signIN.backgroundColor = .systemOrange
+        self.signUp.backgroundColor = .systemOrange
+    }
 
-        
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 1.0 , delay: 0.5,
+        usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [],
+        animations: { [self] in
+            self.welcomeLabel.center.x += 10
+            self.smallLabel.center.x += 10
+          self.signIN.center.y -= 5.0
+          self.signIN.alpha = 1.0
+            self.signUp.center.y -= 5.0
+            self.signUp.alpha = 1.0
+            self.socialLogins.center.y -= 5.0
+            self.socialLogins.alpha = 1.0
+            self.signIN.backgroundColor = .systemIndigo
+            self.signUp.backgroundColor = .systemIndigo
+        },
+        completion: nil)
     }
     
     @IBAction func signInClicked(_ sender: Any) {
